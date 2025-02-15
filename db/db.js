f37babcca02e104bd377aa6872ea52eaa077313d
Priyanka Sharma <priyanka.sharma@digimantra.com>
2025-02-15 16:39:11 +0530

    some fixes has done before live

--- db/db.js
+++ db/db.js
@@ -9,9 +9,15 @@ dotenv.config();
 //   logging: false
 // });
 
-const Model = new Sequelize("playerswanted",  "root",  "root", {
-  host:  "127.0.0.1",
-  dialect: "mysql",
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST;
+const DB_DIALECT = process.env.DB_DIALECT;
+
+const Model = new Sequelize(DB_NAME,  DB_USER,  DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: DB_DIALECT,
   logging: true
 });
 
